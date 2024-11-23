c94b189e7b86d41d4a78aecba9a29a3053cbd6b2
Mohankkanth <mohankkanth2002@gmail.com>
2024-11-23 22:30:06 +0530

    config changed

--- .history/vite.config_20241101213334.js
+++ .history/vite.config_20241101213334.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
+export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-  },
-};
-
+})
--- .history/vite.config_20241123222852.js
+++ .history/vite.config_20241123222852.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+export default defineConfig({
+  
+})
 export default {
-  plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
   },
